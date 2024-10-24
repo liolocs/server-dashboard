2cc4f4b5b543cc2adb8d04c616cde32e7238805e
Olivier Colas <olivercolas.oc@gmail.com>
2024-10-24 13:48:37 +0700

    chore: removed unnecessary component

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -52,11 +52,6 @@ export default buildConfig({
     }
   },
   admin: {
-    components: {
-      // The `BeforeLogin` component renders a message that you see while logging into your admin panel.
-      // Feel free to delete this at any time. Simply remove the line below and the import `BeforeLogin` statement on line 15.
-      beforeLogin: ['@/components/BeforeLogin'],
-    },
     importMap: {
       baseDir: path.resolve(dirname),
     },
